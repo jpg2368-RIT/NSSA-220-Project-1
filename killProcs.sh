3872b73e1a33f302a5926136e93fc0366f7c3d0a
Joey Guarino <113531949+jpg2368-RIT@users.noreply.github.com>
2023-10-25 14:55:22 -0400

    Add files via upload

--- killProcs.sh
+++ killProcs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kill $(ps -a | grep APM | cut -d " " -f 1)
+kill $(ps -a | grep "APM" | cut -d " " -f 1)
 echo "Killed all APM scripts"
--- project1.sh
+++ project1.sh
@@ -3,78 +3,77 @@
 # starts each of the apm scripts
 runProcs () {
 	./APM1 192.168.122.1 &
-	#p1job=$(jobs | grep APM1 | cut -b 2)
-	p1pid=$(ps | grep APM1 | cut -d " " -f 1)
+	p1pid=$!
+	echo "APM1: $p1pid"
 	./APM2 127.0.0.1 &
-	#p2job=$(jobs | grep APM2 | cut -b 2)
-	p2pid=$(ps | grep APM2 | cut -d " " -f 1)
+	p2pid=$!
+	echo "APM2: $p2pid"
 	./APM3 127.0.0.1 &
-	#p3job=$(jobs | grep APM3 | cut -b 2)
-	p3pid=$(ps | grep APM3 | cut -d " " -f 1)
+	p3pid=$!
+	echo "APM3: $p3pid"
 	./APM4 127.0.0.1 &
-	#p4job=$(jobs | grep APM4 | cut -b 2)
-	p4pid=$(ps | grep APM4 | cut -d " " -f 1)
+	p4pid=$!
+	echo "APM4: $p4pid"
 	./APM5 127.0.0.1 &
-	#p5job=$(jobs | grep APM5 | cut -b 2)
-	p5pid=$(ps | grep APM5 | cut -d " " -f 1)
+	p5pid=$!
+	echo "APM5: $p5pid"
 	./APM6 127.0.0.1 &
-	#p6job=$(jobs | grep APM6 | cut -b 2)
-	p6pid=$(ps | grep APM6 | cut -d " " -f 1)
+	p6pid=$!
+	echo "APM6: $p6pid"
 
 }
 
 # kills each of the apm scripts
 killProcs () {
-	kill "$p1pid"
-	kill "$p2pid"
-	kill "$p3pid"
-	kill "$p4pid"
-	kill "$p5pid"
-	kill "$p6pid"
+	#kill "$p1pid"
+	#kill "$p2pid"
+	#kill "$p3pid"
+	#kill "$p4pid"
+	#kill "$p5pid"
+	#kill "$p6pid"
+	kill $(ps -a | grep APM | cut -d " " -f 1)
 	exit 0
 
 }
 
 #collects process level metrics
 procLvlCol () {
-	# cpu util (ps)
-	# ram util (ps)
-	# drive writes (df)
-	# output cpu/ram util to <proc_name>_metrics.csv
+	# cpu util (ps) [DONE]
+	# ram util (ps) [DONE]
+	# drive writes (df) [UNFINISHED]
+	# output cpu/ram util to <proc_name>_metrics.csv [DONE]
 	# format <seconds>,<%cpu>,<%ram>
-	cpu1=$(ps -au | grep APM1 | head -n 1 | cut -d " " -f 6)
-	ram1=$(ps -au | grep APM1 | head -n 1 | cut -d " " -f 8)
+	cpu1=$(ps -p $p1pid -o %cpu | tail -1 | xargs)	
+	ram1=$(ps -p $p1pid -o %mem | tail -1 | xargs)
 	echo "$SECONDS,$cpu1,$ram1" >> APM1_metrics.csv
 	
-	local cpu2=$(ps -au | grep APM2 | head -n 1 | cut -d " " -f 6)
-	local ram2=$(ps -au | grep APM2 | head -n 1 | cut -d " " -f 8)
+	cpu2=$(ps -p $p2pid -o %cpu | tail -1 | xargs)	
+	ram2=$(ps -p $p2pid -o %mem | tail -1 | xargs)
 	echo "$SECONDS,$cpu2,$ram2" >> APM2_metrics.csv
 
-	local cpu3=$(ps -au | grep APM3 | head -n 1 | cut -d " " -f 6)
-	local ram3=$(ps -au | grep APM3 | head -n 1 | cut -d " " -f 8)
+	local cpu3=$(ps -p $p3pid -o %cmd | tail -1 | xargs)	
+	local ram3=$(ps -p $p3pid -o %mem | tail -1 | xargs)
 	echo "$SECONDS,$cpu3,$ram3" >> APM3_metrics.csv
 
-	local cpu4=$(ps -au | grep APM4 | head -n 1 | cut -d " " -f 6)
-	local ram4=$(ps -au | grep APM4 | head -n 1 | cut -d " " -f 8)
+	cpu4=$(ps -p $p4pid -o %cpu | tail -1 | xargs)	
+	ram4=$(ps -p $p4pid -o %mem | tail -1 | xargs)
 	echo "$SECONDS,$cpu4,$ram4" >> APM4_metrics.csv
 
-	local cpu5=$(ps -au | grep APM5 | head -n 1 | cut -d " " -f 6)
-	local ram5=$(ps -au | grep APM5 | head -n 1 | cut -d " " -f 8)
+	cpu5=$(ps -p $p5pid -o %cpu | tail -1 | xargs)	
+	ram5=$(ps -p $p5pid -o %mem | tail -1 | xargs)
 	echo "$SECONDS,$cpu5,$ram5" >> APM5_metrics.csv
 
-	local cpu6=$(ps -au | grep APM6 | head -n 1 | cut -d " " -f 6)
-	local ram6=$(ps -au | grep APM6 | head -n 1 | cut -d " " -f 8)
-	echo "$SECONDS,$cpu6,$ram6" >> APM6_metrics.csv
-	:
-	
+	cpu6=$(ps -p $p6pid -o %cpu | tail -1 | xargs)	
+	ram6=$(ps -p $p6pid -o %mem | tail -1 | xargs)
+	echo "$SECONDS,$cpu6,$ram6" >> APM6_metrics.csv	
 }
 
 # collects system level metrics
 sysLvlCol () {
-	# network bandwith util
-	# drive access rates
-	# drive util	
-	# write to system_metrics.csv
+	# network bandwith util [UNFINISHED]
+	# drive access rates [UNFINISHED]
+	# drive util [UNFINISHED]
+	# write to system_metrics.csv [UNFINISHED]
 	# format <seconds>,<rx data rate>,<tx data rate>,<disk writes>,<available disk capacity>
 	:
 }
@@ -86,8 +85,11 @@ runProcs
 # main loop
 while [[ true ]]
 do
-	procLvlCol
-	sysLvlCol
-	sleep 5
+	if (( $SECONDS % 5 == 0 ))
+	then	
+		procLvlCol
+		sysLvlCol
+		sleep 2
+	fi
 done
 trap killProcs EXIT 2> /dev/null
